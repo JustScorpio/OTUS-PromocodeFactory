85826b8bfb3831a67b45a95e6248957f9861e176
Stanislaw <skorpiello@gmail.com>
2023-10-07 22:58:46 +0500

    Первый тест готов

---
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/PartnersFixture.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Otus.Teaching.PromoCodeFactory.WebHost.Controllers;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Moq;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+{
+    public class PartnersFixture
+    {
+        public PartnersController partnersController { get; private set; }
+
+        public PartnersFixture()
+        {
+            Mock<IRepository<Partner>> partnerRepoMock = new Mock<IRepository<Partner>>();
+            partnersController = new PartnersController(partnerRepoMock.Object);
+        }
+    }
+}
--- src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -1,7 +1,48 @@
-﻿namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+﻿using AutoFixture;
+using System;
+using Moq;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Xunit;
+using Otus.Teaching.PromoCodeFactory.WebHost.Controllers;
+using Otus.Teaching.PromoCodeFactory.WebHost.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
 {
-    public class SetPartnerPromoCodeLimitAsyncTests
+    public class SetPartnerPromoCodeLimitAsyncTests : IClassFixture<PartnersFixture>
     {
-        //TODO: Add Unit Tests
+        PartnersController partnersController;
+
+        public SetPartnerPromoCodeLimitAsyncTests(PartnersFixture fixture) 
+        {
+            partnersController = fixture.partnersController;
+        }
+
+        [Fact]
+        public async void SetPartnerPromoCodeLimitAsync_Partner_Not_Found_By_Guid()
+        {
+            var guid = Guid.NewGuid(); //random non-equivalent-to-existing guid
+            var autoFixture = new Fixture();
+            var request = autoFixture.Build<SetPartnerPromoCodeLimitRequest>().Create();
+
+            var result = await partnersController.SetPartnerPromoCodeLimitAsync(guid, request);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        //[Fact]
+        //public async void SetPartnerPromoCodeLimitAsync_Partner_IsActive_false()
+        //{
+        //    partnersController.;
+        //    var guid = Guid.NewGuid(); //random non-equivalent-to-existing guid
+        //    var autoFixture = new Fixture();
+        //    var request = autoFixture.Build<SetPartnerPromoCodeLimitRequest>().Create();
+
+        //    var result = await partnersController.SetPartnerPromoCodeLimitAsync(guid, request);
+
+        //    Assert.IsType<NotFoundResult>(result);
+        //}
     }
 }
