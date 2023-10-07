390b493c01fcab375d3763c80b7f8849260b2f66
Stanislaw <skorpiello@gmail.com>
2023-10-08 01:00:39 +0500

    2 тест

--- src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -8,6 +8,7 @@ using Otus.Teaching.PromoCodeFactory.WebHost.Controllers;
 using Otus.Teaching.PromoCodeFactory.WebHost.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
 {
@@ -24,25 +25,36 @@ namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
         public async void SetPartnerPromoCodeLimitAsync_Partner_Not_Found_By_Guid()
         {
             var guid = Guid.NewGuid(); //random non-equivalent-to-existing guid
+
             var autoFixture = new Fixture();
-            var request = autoFixture.Build<SetPartnerPromoCodeLimitRequest>().Create();
+            var request = autoFixture.Create<SetPartnerPromoCodeLimitRequest>();
 
             var result = await partnersController.SetPartnerPromoCodeLimitAsync(guid, request);
 
             Assert.IsType<NotFoundResult>(result);
         }
 
-        //[Fact]
-        //public async void SetPartnerPromoCodeLimitAsync_Partner_IsActive_false()
-        //{
-        //    partnersController.;
-        //    var guid = Guid.NewGuid(); //random non-equivalent-to-existing guid
-        //    var autoFixture = new Fixture();
-        //    var request = autoFixture.Build<SetPartnerPromoCodeLimitRequest>().Create();
+        [Fact]
+        public async void SetPartnerPromoCodeLimitAsync_Partner_Found_By_Guid_IsActive_false()
+        {
+            var autoFixture = new Fixture();
+            //Evade recursion
+            autoFixture.Customize<Partner>(x => x.With(par => par.PartnerLimits, new List<PartnerPromoCodeLimit>()));
 
-        //    var result = await partnersController.SetPartnerPromoCodeLimitAsync(guid, request);
+            //Need custom controller with set up private repo in it
+            var inactivePartner = autoFixture.Create<Partner>();
+            inactivePartner.IsActive = false;
+            var repoMock = new Mock<IRepository<Partner>>();
+            repoMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(inactivePartner);
+            var customController = new PartnersController(repoMock.Object);
 
-        //    Assert.IsType<NotFoundResult>(result);
-        //}
+            var guid = Guid.NewGuid(); //random non-equivalent-to-existing guid
+
+            var request = autoFixture.Build<SetPartnerPromoCodeLimitRequest>().Create();
+
+            var result = await customController.SetPartnerPromoCodeLimitAsync(guid, request);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
     }
 }
