2179d1d18e725791319e347c21599e96a9c6911e
Stanislaw <skorpiello@gmail.com>
2023-10-08 17:03:12 +0500

    3 тест

--- src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -48,13 +48,43 @@ namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
             repoMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(inactivePartner);
             var customController = new PartnersController(repoMock.Object);
 
-            var guid = Guid.NewGuid(); //random non-equivalent-to-existing guid
+            var guid = Guid.NewGuid(); //random guid
 
             var request = autoFixture.Build<SetPartnerPromoCodeLimitRequest>().Create();
 
             var result = await customController.SetPartnerPromoCodeLimitAsync(guid, request);
 
             Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(10)]
+        public async void SetPartnerPromoCodeLimitAsync_Set_NumberIssuedPromoCodes_Zero(int numberIssuedPromoCodes)
+        {
+            var autoFixture = new Fixture();
+            autoFixture.Customize<Partner>(x => x.With(par => par.PartnerLimits, new List<PartnerPromoCodeLimit>()));
+            var partner = autoFixture.Create<Partner>();
+            autoFixture.Customize<PartnerPromoCodeLimit>(x => x.With(ppcl => ppcl.Partner, partner));
+            var partnerLimit = autoFixture.Create<PartnerPromoCodeLimit>();
+            partner.IsActive = true;
+            partner.PartnerLimits.Add(partnerLimit);
+            partner.NumberIssuedPromoCodes = numberIssuedPromoCodes;
+            partnerLimit.CancelDate = null;
+
+            //Need custom controller with set up private repo in it
+            var repoMock = new Mock<IRepository<Partner>>();
+            repoMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(partner);
+            var customController = new PartnersController(repoMock.Object);
+
+            var guid = Guid.NewGuid(); //random guid
+
+            autoFixture.Customize<SetPartnerPromoCodeLimitRequest>(x => x.With(req => req.Limit,10));
+            var request = autoFixture.Build<SetPartnerPromoCodeLimitRequest>().Create();
+
+            await customController.SetPartnerPromoCodeLimitAsync(guid, request);
+
+            Assert.Equal(0, partner.NumberIssuedPromoCodes);
         }
     }
 }
